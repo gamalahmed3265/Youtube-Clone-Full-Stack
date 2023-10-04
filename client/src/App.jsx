2d3e071aa7660e899a55fec6b23f027a7f6a3a44
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-10-04 18:11:28 +0300

    add init app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+
+
 function App() {
-  return <div>Hello World!</div>;
+  return <Container>
+    <Menu/>
+    <Main>
+      <Navbar/>
+    </Main>
+  </Container>;
 }
 
 export default App;
