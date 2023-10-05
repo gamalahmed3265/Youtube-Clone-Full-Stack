8f36b4c96db42257ae7888cbfc6bd4743778bd22
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-10-05 12:15:54 +0300

    add dot env

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app=express();
+dotenv.config();
+
 
 app.get("/",(req,res)=>{
-    // console.log();
+    console.log(process.env.mongoConnectId);
     // res.json(req.query);
 })
+
 app.listen(8800,()=>{
     console.log("listen");
 })
