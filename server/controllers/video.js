2737d4497eef596fcaab3ae41feb63d2089cd807
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-10-06 18:11:41 +0300

    finish controlle Video

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,109 +1,182 @@
 import { createError } from "../err.js";
+import User from "../models/User.js";
 import Video from "../models/Video.js";
 
-export const addVideo=async(req,res,next)=>{
-    const newVideo=new Video({
-        userId:req.user.id,
-        ...req.body});
-    try{
+export const addVideo = async (req, res, next) => {
+    const newVideo = new Video({
+        userId: req.user.id,
+        ...req.body
+    });
+    try {
         await newVideo.save();
         res.status(200).json({
             success: true,
             status: 200,
             message: "The video has been Added.",
-            video:newVideo
+            video: newVideo
         });
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 }
-export const updateVideo =async(req,res,next)=>{
-    try{
-        const video=await Video.findById(req.params.id);
+export const updateVideo = async (req, res, next) => {
+    try {
+        const video = await Video.findById(req.params.id);
         if (!video) {
             return next(createError(404, "Video not found!"))
         }
-        if (req.user.id===video.userId) {
-            const updateVideo=await Video.findByIdAndUpdate(req.params.id,
-                {$set :req.body},{
-                    new:true
-                }
-                );
+        if (req.user.id === video.userId) {
+            const updateVideo = await Video.findByIdAndUpdate(req.params.id,
+                { $set: req.body }, {
+                new: true
+            }
+            );
             res.status(200).json({
                 success: true,
                 status: 200,
                 message: "The video has been Updated.",
-                video:updateVideo
+                video: updateVideo
             });
-        }else{
+        } else {
             return next(createError(403, "You can update only your video!"));
         }
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 }
-export const deleteVideo  =async(req,res,next)=>{
-    try{
-        const video=await Video.findById(req.params.id);
+export const deleteVideo = async (req, res, next) => {
+    try {
+        const video = await Video.findById(req.params.id);
         if (!video) {
             return next(createError(404, "Video not found!"))
         }
-        if (req.user.id===video.userId) {
-            const updateVideo=await Video.findByIdAndUpdate(req.params.id,
-                {$set :req.body},{
-                    new:true
-                }
-                );
+        if (req.user.id === video.userId) {
+            await Video.findByIdAndDelete(req.params.id
+            );
             res.status(200).json({
                 success: true,
                 status: 200,
-                message: "The video has been Updated.",
-                video:updateVideo
+                message: "The video has been Deleted.",
             });
-        }else{
+        } else {
             return next(createError(403, "You can update only your video!"));
         }
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 }
-export const addView=async(req,res,next)=>{
+// find
+export const getVideo = async (req, res, next) => {
+    try {
+        const video = await Video.findById(req.params.id);
+        if (!video) {
+            return next(createError(404, "Video not found!"))
+        }
+        res.status(200).json({
+            success: true,
+            status: 200,
+            message: "The video has been Finded.",
+            video: video
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+export const addView = async (req, res, next) => {
+    try {
+        const video = await Video.findByIdAndUpdate(req.params.id,
+            { $inc: { views: 1 } },
+        );
+        if (!video) {
+            return next(createError(404, "Video not found!"))
+        }
+        res.status(200).json({
+            success: true,
+            status: 200,
+            message: "The view has been increased.",
+            video: video
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+export const random = async (req, res, next) => {
+    try {
+        const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+        if (!videos) {
+            return next(createError(404, "Video not found!"))
+        }
+        res.status(200).json({
+            success: true,
+            status: 200,
+            message: "The Videos has been Finded.",
+            videos: videos
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+export const trend = async (req, res, next) => {
+    try {
+        const videos = await Video.find().sort({ views: -1 });
+        if (!videos) {
+            return next(createError(404, "Video not found!"))
+        }
+        res.status(200).json({
+            success: true,
+            status: 200,
+            message: "The Videos has been Finded.",
+            videos: videos
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+export const sub = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.id);
+        const subscribedChannels = user.subscribedUsers;
 
-}
-export const getByTag=(req,res,next)=>{
-    console.log("dfd");
-    res.json({
-        message:"Its Successful"
-    });
-}
-export const getVideo=(req,res,next)=>{
-    console.log("dfd");
-    res.json({
-        message:"Its Successful"
-    });
-}
-export const random=(req,res,next)=>{
-    console.log("dfd");
-    res.json({
-        message:"Its Successful"
-    });
-}
-export const search=(req,res,next)=>{
-    console.log("dfd");
-    res.json({
-        message:"Its Successful"
-    });
-}
-export const sub=(req,res,next)=>{
-    console.log("dfd");
-    res.json({
-        message:"Its Successful"
-    });
-}
-export const trend=(req,res,next)=>{
-    console.log("dfd");
-    res.json({
-        message:"Its Successful"
-    });
+        const list = await Promise.all(
+            subscribedChannels.map(async (channelId) => {
+                return await Video.find({ userId: channelId });
+            })
+        );
+
+        res.status(200).json(list.flat().sort((a, b) => b.createdAt - a.createdAt));
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getByTag = async (req, res, next) => {
+    const tags = req.query.tags.split(",");
+    try {
+        const videos = await Video.find({ tags: { $in: tags } }).limit(20);
+        res.status(200).json(videos);
+        res.status(200).json({
+            success: true,
+            status: 200,
+            videos: videos
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+export const search = async (req, res, next) => {
+    try {
+        const query = req.query.q;
+        const videos = await Video.find({
+            title: { $regex: query, $options: "i" },
+        }).limit(40);
+
+        res.status(200).json({
+            success: true,
+            status: 200,
+            videos: videos
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
--- server/router/video.js
+++ server/router/video.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addVideo,updateVideo, deleteVideo  ,addView, getByTag, getVideo, random, search, sub, trend } from "../controllers/video.js";
+import { addVideo,updateVideo, deleteVideo ,getVideo,addView, getByTag,  random, search, sub, trend } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router=express.Router();
@@ -10,8 +10,8 @@ router.put("/:id", verifyToken, updateVideo )
 router.delete("/:id", verifyToken, deleteVideo )
 router.get("/find/:id", getVideo)
 router.put("/view/:id", addView)
-router.get("/trend", trend)
 router.get("/random", random)
+router.get("/trend", trend)
 router.get("/sub",verifyToken, sub)
 router.get("/tags", getByTag)
 router.get("/search", search)
